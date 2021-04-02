095a62bda1c9106524ac7fe8ad596270964cf58c
pravin desai <pravindesai100@gmail.com>
2021-04-02 08:41:31 +0530

    new interview question added

--- src/Common_Interview_Programs/Reverse_a_String.java
+++ src/Common_Interview_Programs/Reverse_a_String.java
@@ -3,10 +3,11 @@ package Common_Interview_Programs;
 public class Reverse_a_String {
     public static void main(String[] args) {
         String str = "ABCDEFGH";
-        String strRev = "";
+        StringBuilder strRev = new StringBuilder();
 
         for (int i = str.length()-1; i>=0; i--){
-            strRev = strRev+str.charAt(i);
+            strRev.append(str.charAt(i));
+            //strRev = strRev+str.charAt(i);
         }
 
         System.out.println("Reversed String : "+strRev);
